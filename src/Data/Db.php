45d8c2142b7dce3ac42d8c595471bb8ef7ae7acf
Malek BK <malekbk98@gmail.com>
2021-10-21 16:30:23 +0100

    Add DB classe to store classes instances

---
+++ src/Data/Db.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Data;
+
+class Db
+{
+    public static $orders = [];
+    public static $recipees = [];
+    public static $pizzaSizes = [];
+    public static $ingredients = [];
+    public static $accounts = [];
+    public static $payements = [];
+}
