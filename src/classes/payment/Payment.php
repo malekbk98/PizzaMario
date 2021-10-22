5156b36794450b3adad41a536c3493f38f34685e
Youssef BAHI <you97ssef@gmail.com>
2021-10-23 00:25:52 +0200

    Refactor Payement

--- src/classes/payment/Payment.php
+++ src/classes/payment/Payment.php
@@ -1,32 +1,13 @@
 <?php
+
 namespace classes\payment;
 
 abstract class Payment
 {
-    public $amount, $date, $orderID;
+    public $amount;
 
-    public function __construct(string $orderID, double $amount)
+    public function __construct(float $amount)
     {
-        $this->orderID = $orderID;
         $this->amount = $amount;
-        $this->date = new DateTime();
-
-    }
-
-    public function __toString()
-    {
-        return json_encode($this);
-    }
-
-    public function __set(string $name, $value)
-    {
-        $this->$name = $value;
-    }
-
-    public function __get(string $name)
-    {
-        return $this->$name;
     }
 }
-
-?>
