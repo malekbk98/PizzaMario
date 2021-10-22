8c2ce129e8a3970024a1f0f99d3c94965043b906
Youssef BAHI <you97ssef@gmail.com>
2021-10-23 00:23:54 +0200

    Chandged name of ReadyRecipee to Product and refactoring..

---
+++ src/classes/recipes/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace classes\recipes;
+
+class Product extends Recipe
+{
+    public $name;
+    public $category;
+
+    public function __construct(string $name, string $category, float $price)
+    {
+        $this->category = $category;
+        $this->name = $name;
+        $this->price = $price;
+    }
+}
